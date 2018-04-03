f741dfad37cd9a6c2c6b4f32fc072fa12bfcf6cf
stella <3291570994@qq.com>
2018-04-03 16:21:43 +0800

    商品详情数据渲染

--- src/pages/index/productdetail.js
+++ src/pages/index/productdetail.js
@@ -7,7 +7,7 @@ import * as Datas from 'api';
 import VueLazyLoad from 'vue-lazyload';
 import {setStore,getStore} from '@js/config';
 import 'src/static/plugins/swiper-3.4.2.min.js';
-
+import {addClass,removeClass} from 'src/static/js/dom.js';
 Vue.use(VueLazyLoad,{
     error:'../static/images/public/loading.gif',
     loading:'../static/images/public/loading.gif'
@@ -24,13 +24,17 @@ new Vue({
     },
     data(){
         return{
-            code:'',
+            userId:'',
+            userCode:'',//站点编号
+            code:'',//商品编号
             name:'',
             ean:'',
             form:'',
             minUnit:'',
             minValue:null,
             price:'',
+            zhongPackage:'',
+            bigPackage:'',
             effectiveDate:'',
             productionDate:'',
             barcode:'',
@@ -40,11 +44,24 @@ new Vue({
             quantityFormat:[],
             realStock:'',
             saleScope:'',
+            otc:'',
             sentUrl:'',
             specifications:'',
             supercategories:'',
-            isShow: false,
+            isShow:false,//是否显示菜单栏
+            isShowDiscount: true,//是否选中折扣商品
             salesPromotion:'',//促销信息
+            medicalDeviceLicense:'',
+            instructions:'',
+            isDiscount:true,//是否是折扣商品
+            discountPrice:'',
+            discountRealStock:'',
+            discountPackage:'',
+            discountBigPackage:'',
+            discountProductionDate:'',
+            discountEffectiveDate:'',
+            discountValidity:'',
+
         };
     },
     beforemount(){
@@ -58,11 +75,11 @@ new Vue({
             window.history.go(-1);
         },
         getDatas(){
-            let $this = this,
-                userId = getStore('user_id'),
-                userCode = getStore('user_code');
+            let $this = this;
+            $this.userId = getStore('user_id') || '',
+            $this.userCode = getStore('user_code') || '';
             //'code='+$this.code+'&uid='+userId+'&companyCode='+userCode
-            request.post(Datas.productDetail,'code='+$this.code,)
+            request.post(Datas.productDetail,'code='+$this.code)
                 .then(res => {
                     //this.specialOfferArray = res.resultArray;
                     console.log(res);
@@ -70,6 +87,7 @@ new Vue({
                     $this.name = res.name;
                     $this.ean = res.ean;
                     $this.form = res.form;
+                    $this.otc = res.otc;
                     $this.minUnit = res.minUnit;
                     $this.minValue = res.minValue;
                     $this.price = res.price;
@@ -87,6 +105,20 @@ new Vue({
                     $this.supercategories = res.supercategories;
                     $this.salesPromotion = res.salesPromotion || '';
                 });
+        },
+        handleChoseNormal(event){
+            let currentTargetDom = event.currentTarget;
+            addClass(currentTargetDom,'active');
+            var siblingDom = document.querySelector('.discountTab');
+            removeClass(siblingDom ,'active');
+            this.isShowDiscount = false;
+        },
+        handleChoseDiscount(event){
+            let currentTargetDom = event.currentTarget;
+            addClass(currentTargetDom,'active');
+            var siblingDom = document.querySelector('.normalTab');
+            removeClass(siblingDom ,'active');
+            this.isShowDiscount = true;
         }
     },
     beforeCreate(){
