2e07b8ab6414d09cce7987ee52ccbd32a5a6a4ec
stella <3291570994@qq.com>
2018-04-09 11:25:45 +0800

    折扣专题判断是否登录

--- src/pages/index/outLet.js
+++ src/pages/index/outLet.js
@@ -8,7 +8,7 @@ import { request } from 'common';
 import * as Datas from 'api';
 import VueLazyLoad from 'vue-lazyload';
 import HeadTop from 'src/common/header.vue';
-import ProductList from './components/topic.vue';
+import {setStore,getStore} from '@js/config';
 
 //懒加载
 Vue.use(VueLazyLoad,{
@@ -25,8 +25,7 @@ new Vue({
     el: '#outLetBox',
     router,
     components:{
-        HeadTop,
-        ProductList
+        HeadTop
     },
     data(){
         return{
@@ -35,15 +34,25 @@ new Vue({
             page:0,
             pageSize: 10,
             companyCode:1000,
-            userId:null
+            userId:null,
+            priceChecked:''
         };
     },
     methods: {
-        getDatas(){
+        initDatas(){
             let $this = this;
             request.post(Datas.getDiscountDepot, 'companyCode='+$this.companyCode).then(res => {
                 console.log(res);
                 $this.outLetArray = res.resultArray;
+                $this.pageSize = res.pageSize;
+            });
+        },
+        getDatas(){
+            let $this = this;
+            request.post(Datas.getDiscountDepot, 'uid='+$this.userId).then(res => {
+                console.log(res);
+                $this.outLetArray = res.resultArray;
+                $this.pageSize = res.pageSize;
             });
         },
         getGoodsList(flag){
@@ -60,6 +69,8 @@ new Vue({
                     this.outLetArray = this.outLetArray.concat(res.resultArray);
                     if(res.resultArray == 0){
                         this.busy = true;
+                        document.querySelector('.loading').innerHTML = '没有更多了';
+                        //console.log(document.querySelector('.loading').innerHTML);
                     }else{
                         this.busy = false;
                     }
@@ -76,13 +87,33 @@ new Vue({
             // 多次加载数据
             setTimeout(() => {
                 this.page ++;
-                //this.getGoodsList(true);
+                this.getGoodsList(true);
             }, 100);
         }
 
     },
     created(){
-        this.getDatas();
+        //获取用户信息
+        let hasUserId = getStore('user_id'),
+            hasuserCode = getStore('user_code');
+        if(hasUserId){
+            this.userId = hasUserId;
+            this.companyCode = hasuserCode;
+            this.getDatas();
+        }else{
+            this.initDatas();
+        }
+
     },
+    mounted(){
+        window.onload = function(){
+            let stock = document.querySelectorAll('.stock');
+            for(let i=0;i<stock.length;i++){
+                var value = stock[i].innerHTML;
+                stock[i].innerHTML = parseFloat(stock[i].innerHTML);
+            }
+        }
+
+    }
 
 });
